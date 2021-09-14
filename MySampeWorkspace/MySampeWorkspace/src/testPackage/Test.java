5ec60e267e13d67155a506ee26c2c4257fa7780d
Sabari Roy <Sabari Roy@Warmachine2>
2021-09-14 14:49:30 +0530

    added some random comments to this Test.java

---
+++ MySampeWorkspace/MySampeWorkspace/src/testPackage/Test.java
@@ -0,0 +1,22 @@
+package testPackage;
+
+//import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		readNumber rn = new readNumber();
+		rn.calculateWord();
+		
+		
+
+	}
+}
+	
+		
+	
+		
+
+
+//commeted 
+//added a new comment to this code
---
+++ MySampeWorkspace/src/testPackage/Test.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+//import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		readNumber rn = new readNumber();
+		rn.calculateWord();
+		
+		
+
+	}
+}
+	
+		
+	
+		
+
+
